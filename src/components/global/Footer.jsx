97614a0aee2bae5c65eb76b03fb0d10e3c99d0a1
Ziiron1 <guilherme_profissional@yahoo.com.br>
2023-01-07 17:47:20 -0300

    Cleaning the code

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import Logo from '../../img/Logo.png'
+import GamingRoom from '../../img/GamingRoom.png'
 
 
 function Footer() {
@@ -11,7 +11,7 @@ function Footer() {
       <div className="container-footer">
         <div className="row-footer">
           <div className="footer-col">
-            <img src={Logo} width="200px" className='logo_company' alt="Logo Company" />
+            <img src={GamingRoom} width="200px" className='logo_company' alt="Logo Company" />
           </div>
           <div className="footer-col">
           </div>
@@ -26,9 +26,12 @@ function Footer() {
           <div className="footer-col">
             <h4>Subscribe!</h4>
             <div className="form-sub">
-              <form>
-                <input type="email" placeholder="Send me an e-mail" required />
+              <form action="https://formcarry.com/s/ChvCzDJuf" method="POST" accept-charset="UTF-8">
+                <input style={{ margin: '10px 0 0px 0' }} type="name" name="Name" placeholder='Your Name' />
+                <input style={{ margin: '10px 0 0px 0' }} type="text" className='texto' name="text" placeholder='Write me a message' />
                 <button className='btn-primary'>Submit</button>
+                <input type="hidden" name="_gotcha" />
+
               </form>
             </div>
 
@@ -44,6 +47,7 @@ function Footer() {
         </div>
       </div>
       <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>&copy;All rights reserved &reg;</h3>
+      <h5 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Api caught on&nbsp;<a href="https://rawg.io" target="_blank">Rawg.io</a> </h5>
     </footer>
   )
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -49,10 +49,10 @@ function GamePage() {
       fetchApi();
     }
 
-
     return () => {
       controller.abort();
     };
+
   }, [query]);
 
   if (loading) return <p>Loading...</p>;
@@ -64,79 +64,33 @@ function GamePage() {
         <div className='margin all_card_game'>
           <div className='platforms_flex'>
             {/* Plataformas */}
-            <h3 style={{ paddingBottom: 7 }}>Plataform(s): </h3>
+            <h3 style={{ paddingBottom: 7 }}>{data.platforms.length > 1 ? 'Plataforms: ' : 'Plataform: '}</h3>
+            {data.platforms.map((platform, index) => (
+              <h3 key={index}
+                style={{
+                  marginTop: 3, marginLeft: 2, fontSize: '1rem'
+                }}>&nbsp;
+                {platform.platform.name}
+                {index < data.platforms.length - 1 && ','}
 
-            {data['platforms']['0'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['0']['platform']['name']}
-                {data['platforms']['1'] && ','}
               </h3>
-            }
-
-            {data['platforms']['1'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['1']['platform']['name']}
-                {data['platforms']['2'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['2'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['2']['platform']['name']}
-                {data['platforms']['3'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['3'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['3']['platform']['name']}
-                {data['platforms']['4'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['4'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['4']['platform']['name']}
-                {data['platforms']['5'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['5'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['5']['platform']['name']}
-                {data['platforms']['6'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['6'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['6']['platform']['name']}
-                {data['platforms']['7'] && ','}
-              </h3>
-            }
-
-            {data['platforms']['7'] &&
-              <h3 style={{ marginTop: 4, fontSize: '.85rem' }}>&nbsp;
-                {data['platforms']['7']['platform']['name']}
-                {data['platforms']['8'] && ','}
-              </h3>
-            }
-
+            ))}
           </div>
+
           {/* Imagem e Descrições*/}
           <div style={{ display: 'flex' }} className="all_rating_data">
             <img src={data.background_image} style={{ borderRadius: 5 }} className="img_api" alt={data.name + ` Image`} />
             <div style={{ display: 'block' }}>
 
               {data.metacritic && (
-                <p style={{ marginLeft: 20 }}>Metacritic score: {data.metacritic}/100</p>
-
+                <>
+                  <strong> <p style={{ marginLeft: 20 }}>Metacritic score:</p> </strong>
+                  <p style={{ marginLeft: 20 }}>{data.metacritic}/100</p>
+                </>
               )}
 
-              <strong><small style={{ marginLeft: 20 }}>Site Rating: {data.rating}/5</small></strong>
-
-              <div className='ratings'>
-                <div className='ratings_div'>
+              <div className='ratings' style={{ marginLeft: 20 }}>
+                <div style={{ paddingTop: 20 }}>
                   <h4>🎯{data['ratings']['0']['title']}</h4>
                   <h4 style={{ fontWeight: 'normal' }}>{data['ratings']['0']['count']} Votes</h4>
                   <h5 style={{ fontWeight: 'normal' }}>{data['ratings']['0']['percent']}%</h5>
@@ -154,7 +108,7 @@ function GamePage() {
                   <h5 style={{ fontWeight: 'normal' }}>{data['ratings']['2']['percent']}%</h5>
                 </div>
                 <br />
-                <div>
+                <div style={{ marginBottom: 20 }}>
                   <h4>🤮{data['ratings']['3']['title']}</h4>
                   <h4 style={{ fontWeight: 'normal' }}>{data['ratings']['3']['count']} Votes</h4>
                   <h5 style={{ fontWeight: 'normal' }}>{data['ratings']['3']['percent']}%</h5>
@@ -169,96 +123,26 @@ function GamePage() {
           {/* Nome do jogo */}
           <p className='game_name'>{data.name}</p>
           <br />
+
           <div style={{ display: 'flex' }}>
             <h4>Genres: </h4>
-
-            {data['genres']['0'] &&
-              <p>&nbsp;
-                {data['genres']['0']['name']}
-                {data['genres']['1'] && ','}
+            {data.genres.map((genre, index) => (
+              <p key={index}>&nbsp; {genre.name}
+                {index !== data.genres.length - 1 ? ',' : ''}
               </p>
-            }
-
-            {data['genres']['1'] &&
-              <p>&nbsp;
-                {data['genres']['1']['name']}
-                {data['genres']['2'] && ','}
-              </p>
-            }
-
-            {data['genres']['2'] &&
-              <p>&nbsp;
-                {data['genres']['2']['name']}
-                {data['genres']['3'] && ','}
-              </p>
-            }
-
-            {data['genres']['3'] &&
-              <p>&nbsp;
-                {data['genres']['3']['name']}
-                {data['genres']['4'] && ','}
-              </p>
-            }
-
-            {data['genres']['4'] &&
-              <p>&nbsp;
-                {data['genres']['4']['name']}
-                {data['genres']['5'] && ','}
-              </p>
-            }
-
-            {data['genres']['5'] &&
-              <p>&nbsp;
-                {data['genres']['5']['name']}
-                {data['genres']['6'] && ','}
-              </p>
-            }
-
-            {data['genres']['6'] &&
-              <p>&nbsp;
-                {data['genres']['6']['name']}
-                {data['genres']['7'] && ','}
-              </p>
-            }
-
-            {data['genres']['7'] &&
-              <p>&nbsp;
-                {data['genres']['7']['name']}
-                {data['genres']['8'] && ','}
-              </p>
-            }
-
-            {data['genres']['8'] &&
-              <p>&nbsp;
-                {data['genres']['8']['name']}
-              </p>
-            }
-
+            ))}
           </div>
+
           <br />
           {/* Desenvolvedores do jogo */}
           <div style={{ display: 'flex' }}>
-            <h3>Developer(s): </h3>
-            {data['developers']['0'] &&
-              <h3 style={{ marginTop: 5, fontSize: '.8rem' }}>&nbsp;
-                {data['developers']['0']['name']}
-                {data['developers']['1'] && ','}
+            <h3>{data.developers.length > 1 ? "Developers: " : "Developer: "}</h3>
+            {data.developers.map((developer, index) => (
+              <h3 key={index} style={{ marginTop: 7, marginLeft: 3, letterSpacing: 1, fontSize: '.8rem' }}>
+                &nbsp;{developer.name}
+                {index !== data.developers.length - 1 ? ',' : ''}
               </h3>
-            }
-
-            {data['developers']['1'] &&
-              <h3 style={{ marginTop: 5, fontSize: '.8rem' }}>&nbsp;
-                {data['developers']['1']['name']}
-                {data['developers']['2'] && ','}
-              </h3>
-            }
-
-            {data['developers']['2'] &&
-              <h3 style={{ marginTop: 5, fontSize: '.8rem' }}>&nbsp;
-                {data['developers']['2']['name']}
-              </h3>
-            }
-
+            ))}
           </div>
 
           <br />
@@ -270,7 +154,7 @@ function GamePage() {
           <br />
 
           {/* Descrição do jogo */}
-          <div className='description_game'>
+          <div className='description_game' style={{ letterSpacing: .8 }}>
             {data.description_raw && (
               <>
                 <span >
@@ -282,76 +166,30 @@ function GamePage() {
             <br />
             {data.website && (
               <>
-                <a style={{ fontSize: '1rem' }} className="website" href={data.website}> <p>Website: {data.website}</p></a>
+                <div style={{ display: 'flex', fontSize: '1rem' }}>
+                  <p>Website:&nbsp; </p>
+                  <a style={{ fontSize: '1rem' }} className="website" href={data.website}>  {data.website}</a>
+                </div>
               </>
             )}
           </div>
 
 
+          <div style={{ marginTop: 25 }}>
 
-          <div style={{ marginTop: 20 }}>
-
-            <span onClick={toggleList} style={{ fontWeight: 'bold', fontSize: '1rem', cursor: 'pointer', padding: '5px', background: '#131313', borderRadius: '5px' }} >Show/Hide Tags</span>
+            <span onClick={toggleList} className="ShowTags">Show/Hide Tags</span>
 
             <ul className='game_tags' style={{ display: showList ? 'flex' : 'none' }}>
 
-              {data['tags']['0'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['0']['name']}
-                  {data['tags']['1'] && ','}
+              {data.tags.slice(0, 8).map((tag, index) => (
+                <p key={index}>
+                  &nbsp;{tag.name}
+                  {index !== data.tags.slice(0, 8).length - 1 ? ',' : ''}
                 </p>
-              }
-
-              {data['tags']['1'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['1']['name']}
-                  {data['tags']['2'] && ','}
-                </p>
-              }
-
-              {data['tags']['2'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['2']['name']}
-                  {data['tags']['3'] && ','}
-                </p>
-              }
-
-              {data['tags']['3'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['3']['name']}
-                  {data['tags']['4'] && ','}
-                </p>
-              }
-
-              {data['tags']['4'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['4']['name']}
-                  {data['tags']['5'] && ','}
-                </p>
-              }
-
-              {data['tags']['5'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['5']['name']}
-                  {data['tags']['6'] && ','}
-                </p>
-              }
-
-              {data['tags']['6'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['6']['name']}
-                  {data['tags']['7'] && ','}
-                </p>
-              }
-
-              {data['tags']['7'] &&
-                <p style={{ fontSize: '1rem' }}>&nbsp;
-                  {data['tags']['7']['name']}
-                  {data['tags']['8'] && ','}
-                </p>
-              }
+              ))}
             </ul>
           </div>
+
           <br />
           <br />
 
